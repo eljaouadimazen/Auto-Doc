d81886bff45af9000ef13dd7bdef6b42a64b9b43
eljaouadimazen <mazen.eljaouadi@esprit.tn>
2026-05-09 00:00:20 +0100

    "coverage test success percentage"

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 15,
-      functions: 20,
-      lines: 20,
-      statements: 20
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30
     }
   }
 };
