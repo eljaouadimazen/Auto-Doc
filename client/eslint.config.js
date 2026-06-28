8793abd40ba6bd04b4049e8b264301dcf97ad299
eljaouadimazen <mazen.eljaouadi@esprit.tn>
2026-06-28 14:40:20 +0100

    hotfix:/ fix the eslint for frontend errors

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,7 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 
 export default [
-  { ignores: ['dist/**', 'node_modules/**', 'public/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'public/**', 'assets/**'] },
   js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
 import path from 'path'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
     },
   },
   plugins: [tailwindcss(), react()],
